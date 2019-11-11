6cd6725cca9552499e11753c8a72d1937e6d2d3a
James Rodriguez <jamesr817@gmail.com>
2019-11-11 00:32:15 -0600

    Come back and play around with the equalsIgnore() and contains method when you can to see how to make them work together in a chain if possible.

--- src/exercises/Alice.java
+++ src/exercises/Alice.java
@@ -4,12 +4,22 @@ import java.util.Scanner;
 public class Alice {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+
         String passage = "Alice was beginning to get very tired of sitting by her sister on the bank, and of having " +
                 "nothing to do: once or twice she had peeped into the book her sister was reading, but it had no " +
                 "pictures or conversations in it, 'and what is the use of a book,' thought Alice 'without pictures or" +
                 "conversation?'";
 
+        String searchTerm;
+
         System.out.println("Enter a term to search for in the passage: ");
+        searchTerm =input.nextLine();
+
+        input.close();
+
+        boolean doesExist = passage.toLowerCase().contains(searchTerm.toLowerCase());
+
+        System.out.print(doesExist);
 
     }
 }
