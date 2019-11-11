b256edb8802b7a8bf24f005054fff0fb2576cdc6
James Rodriguez <jamesr817@gmail.com>
2019-11-11 02:58:09 -0600

    Studio 1 of unit 2 Area of a Circle completed

--- src/org/launchcode/java/studios/areaofacircle/Area.java
+++ src/org/launchcode/java/studios/areaofacircle/Area.java
@@ -1,20 +1,21 @@
 package org.launchcode.java.studios.areaofacircle;
 import java.util.Scanner;
-import java.lang.Math;
+import java.lang.Math.*;
 
 public class Area {
     public static void main(String[] args) {
         double radius;
+        double pi = Math.PI;
         double area;
         Scanner input;
+
 
         input = new Scanner(System.in);
         System.out.println("Enter a radius: ");
         radius = input.nextDouble();
-    }
+        input.close();
+//        area = pi * radius * radius;
 
-    public static Double getArea(Double radius) {
-        Double pi = Math.PI;
-        return pi * radius * radius;
+        System.out.println(Circle.getArea(radius));
     }
 }
